8d3ce1ddf7660dc7c3b614082fd81e1ef00179a8
qiannuanya <119518656+qiannuanya@users.noreply.github.com>
2021-04-18 21:38:18 +0000

    automatically generated from GPLed version

--- code/nn_module.py
+++ code/nn_module.py
@@ -4,3 +4,27 @@ import tensorflow as tf
 
 """
 https://explosion.ai/blog/deep-learning-formula-nlp
+embed -> encode -> attend -> predict
+"""
+def batch_normalization(x, training, name):
+    bn_train = tf.layers.batch_normalization(x, training=True, reuse=None, name=name)
+    bn_inference = tf.layers.batch_normalization(x, training=False, reuse=True, name=name)
+    z = tf.cond(training, lambda: bn_train, lambda: bn_inference)
+    return z
+
+
+#### Step 1
+def embed(x, size, dim, seed=0, flatten=False, reduce_sum=False):
+    # std = np.sqrt(2 / dim)
+    std = 0.001
+    minval = -std
+    maxval = std
+    emb = tf.Variable(tf.random_uniform([size, dim], minval, maxval, dtype=tf.float32, seed=seed))
+    # None * max_seq_len * embed_dim
+    out = tf.nn.embedding_lookup(emb, x)
+    if flatten:
+        out = tf.layers.flatten(out)
+    if reduce_sum:
+        out = tf.reduce_sum(out, axis=1)
+    return out
+
