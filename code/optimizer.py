c36df1f4ea2f9b13ecdfcb33c292bab9054d01df
qiannuanya <119518656+qiannuanya@users.noreply.github.com>
2021-06-06 06:25:43 +0000

    construction

---
+++ code/optimizer.py
@@ -0,0 +1,14 @@
+
+
+"""
+https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&cad=rja&uact=8&ved=0ahUKEwih7-6VlejYAhWGS98KHWeLCWQQFgg3MAE&url=https%3A%2F%2Fwww.bigdatarepublic.nl%2Fcustom-optimizer-in-tensorflow%2F&usg=AOvVaw3jmxRDqr2pkGRLvX6rNJrl
+"""
+
+import tensorflow as tf
+from tensorflow.python.framework import constant_op
+from tensorflow.python.ops import random_ops
+from tensorflow.python.eager import context
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import math_ops
