73dd48f26be9143a3c75cb7a5b72f56a8dc8ea6e
qiannuanya <119518656+qiannuanya@users.noreply.github.com>
2021-07-11 23:05:36 +0000

    ??

--- code/topk.py
+++ code/topk.py
@@ -14,3 +14,19 @@ class BucketSort(object):
                 counts[w] += 1
 
         buckets = [[]] * (sum(counts.values()) + 1)
+        for i, count in counts.items():
+            buckets[count].append(i)
+
+        result = []
+        # result_append = result.append
+        for i in reversed(range(len(buckets))):
+            for j in range(len(buckets[i])):
+                # slower
+                # result_append(buckets[i][j])
+                result.append(buckets[i][j])
+                if len(result) == k:
+                    return result
+        return result
+
+
+# Quick Select
