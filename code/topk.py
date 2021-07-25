39c2c41aaa63045b23e6cf2e10cbfd3a5aa15264
qiannuanya <119518656+qiannuanya@users.noreply.github.com>
2021-07-25 09:20:07 +0000

    drm/radeon: fix resume on some rs4xx boards (v2)

--- code/topk.py
+++ code/topk.py
@@ -41,3 +41,15 @@ class QuickSelect(object):
         """
         counts = defaultdict(int)
         for ws in words:
+            for w in ws:
+                counts[w] += 1
+        p = []
+        for key, val in counts.items():
+            p.append((-val, key))
+        self.kthElement(p, k)
+
+        result = []
+        sorted_p = sorted(p[:k])
+        for i in range(k):
+            result.append(sorted_p[i][1])
+        return result
