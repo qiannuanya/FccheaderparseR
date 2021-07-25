c9be8e7a03a94cf644f5faf7a8637031407908de
qiannuanya <119518656+qiannuanya@users.noreply.github.com>
2021-07-25 16:31:22 +0000

    add npm ignore

--- code/topk.py
+++ code/topk.py
@@ -53,3 +53,19 @@ class QuickSelect(object):
         for i in range(k):
             result.append(sorted_p[i][1])
         return result
+
+    def kthElement(self, nums, k):  # O(n) on average
+        def PartitionAroundPivot(left, right, pivot_idx, nums):
+            pivot_value = nums[pivot_idx]
+            new_pivot_idx = left
+            nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+            for i in range(left, right):
+                if nums[i] < pivot_value:
+                    nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
+                    new_pivot_idx += 1
+
+            nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
+            return new_pivot_idx
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
